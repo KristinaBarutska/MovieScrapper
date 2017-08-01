4a8181d252dec63fa377d15c8d2396346307d241
Kristina Barutska <k.barutska@proxiad.com>
2017-08-01 09:40:16 +0300

    Initial commit

---
+++ MovieScrapper.Business/CategoryService.cs
@@ -0,0 +1,19 @@
+﻿using MovieScrapper.Data;
+using MovieScrapper.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieScrapper.Business
+{
+    public class CategoryService
+    {
+        public IEnumerable<MovieCategory> GetAll()
+        {
+            var repo = new CategoryRepository();
+            return repo.GetAll();
+        }
+    }
+}
---
+++ MovieScrapper.Data/CategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using MovieScrapper.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieScrapper.Data
+{
+    public class CategoryRepository
+    {
+        public IEnumerable<MovieCategory> GetAll()
+        {
+
+            using (var ctx = new MovieContext())
+            {
+                var databaseCategory = ctx.MovieCaterogries.Include("Movies").ToList();
+                return databaseCategory;
+            }
+        }
+    }
+}
